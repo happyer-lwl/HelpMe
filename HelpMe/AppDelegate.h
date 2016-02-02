25a6d7e1db3ee21e15c41f78ec44ba8c2bcc0404
happyer-lwl <runlwl@163.com>
2016-02-02 17:32:02 +0800

    Initial Commit

---
+++ HelpMe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelpMe
+//
+//  Created by 刘伟龙 on 16/2/2.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelpMe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelpMe
+//
+//  Created by 刘伟龙 on 16/2/2.
+//  Copyright © 2016年 lwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
